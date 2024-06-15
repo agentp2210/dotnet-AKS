75b082467a1a994cf5065ee6f65fb40414253a2b
An Tran <atra@gmail.com>
2024-06-15 20:18:47 +0200

    update script ACR

--- scripts/push-to-ACR.sh
+++ scripts/push-to-ACR.sh
@@ -5,41 +5,21 @@ acr_url=$(az acr list --query "[].loginServer" -o tsv)
 az acr login --name $acr_url
 
 # Remove image if exist
-existing_image=$(docker images | grep azurecr | awk '{print $1}')
-if [[ ! -z "$existing_image" ]]
+existing_images=$(docker images | grep azurecr | awk '{print $1}')
+if [[ ! -z "$existing_images" ]]
 then
-    docker rmi $existing_image
+    docker rmi $existing_images
 fi
 
 # Build images if not exist
 cd ../code
 
-existing_images=$(docker images | grep springcommunity | awk '{print $1}')
+existing_image=$(docker images | grep petclinic | awk '{print $1}')
 
-if [ -z "$existing_images" ]; then
-    ./mvnw clean install -P buildDocker
+if [ -z "$existing_image" ]; then
+    docker build -t petclinic . -f Dockerfile.multi
 fi
 
 # Push to ACR
-cd ../code
-
-docker tag springcommunity/spring-petclinic-api-gateway:latest $acr_url/springcommunity/spring-petclinic-api-gateway:latest
-docker push $acr_url/springcommunity/spring-petclinic-api-gateway:latest
-
-docker tag springcommunity/spring-petclinic-discovery-server:latest $acr_url/springcommunity/spring-petclinic-discovery-server:latest
-docker push $acr_url/springcommunity/spring-petclinic-discovery-server:latest
-
-docker tag springcommunity/spring-petclinic-config-server:latest $acr_url/springcommunity/spring-petclinic-config-server:latest
-docker push $acr_url/springcommunity/spring-petclinic-config-server:latest
-
-docker tag springcommunity/spring-petclinic-visits-service:latest $acr_url/springcommunity/spring-petclinic-visits-service:latest
-docker push $acr_url/springcommunity/spring-petclinic-visits-service:latest
-
-docker tag springcommunity/spring-petclinic-vets-service:latest $acr_url/springcommunity/spring-petclinic-vets-service:latest
-docker push $acr_url/springcommunity/spring-petclinic-vets-service:latest
-
-docker tag springcommunity/spring-petclinic-customers-service:latest $acr_url/springcommunity/spring-petclinic-customers-service:latest
-docker push $acr_url/springcommunity/spring-petclinic-customers-service:latest
-
-docker tag springcommunity/spring-petclinic-admin-server:latest $acr_url/springcommunity/spring-petclinic-admin-server:latest
-docker push $acr_url/springcommunity/spring-petclinic-admin-server:latest
+docker tag petclinic:latest $acr_url/petclinic:latest
+docker push $acr_url/petclinic:latest
