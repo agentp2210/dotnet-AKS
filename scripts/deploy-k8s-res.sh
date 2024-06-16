2aae75e9fd6cb3f2f25a49ca9e895f4d42266348
An Tran <atra@gmail.com>
2024-06-16 10:44:46 +0200

    update k8s manifests

--- scripts/deploy-k8s-res.sh
+++ scripts/deploy-k8s-res.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+cd ../k8s
+
 # Create db
 kubectl apply -f mysql-config-persistentvolumeclaim.yaml
 kubectl apply -f mysql-data-persistentvolumeclaim.yaml
@@ -8,7 +10,7 @@ kubectl apply -f mysql-deployment.yaml
 kubectl apply -f mysql-service.yaml
 
 # Create petclinic
-acr_url=$(az acr list --query "[].name" -o tsv)
+acr_url=$(az acr list --query "[].loginServer" -o tsv)
 
 sed -e "s/111122223333.azurecr.io/$acr_url/g" "petclinic-deployment.yaml" | kubectl apply -f -
 kubectl apply -f petclinic-service.yaml
