94650c99b26c6de66814970d4ab0f9629cde67a6
An Tran <atra@gmail.com>
2024-10-31 13:34:59 +0100

    update argocd script

--- scripts/argocd.sh
+++ scripts/argocd.sh
@@ -3,17 +3,28 @@ cd "$(dirname "$0")"
 
 # Deploy Argo CD
 echo "Installing Argo CD"
+argocd_host="argocd.anhalan.nl"
 helm upgrade --install -n argocd --create-namespace --set server.ingress.enabled="true" \
---set server.ingress.ingressClassName="nginx" --set global.domain="argocd.anhalan.nl" \
+--set server.ingress.ingressClassName="nginx" --set global.domain=$argocd_host \
 --set configs.params."server\.insecure"=true argo-cd oci://ghcr.io/argoproj/argo-helm/argo-cd
 
+sleep 10
+
+# Check if argo cd is ready
+status_code=$(curl -I $argocd_host 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+if [ $status_code != 200 ]; then
+    sleep 30
+fi
+
 # Create ArgoCD apps
+acr_url=$(az acr list --query "[].loginServer" -o tsv)
 argocd_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-argocd login argocd.anhalan.nl --insecure --username admin --password $argocd_password
+
+argocd login $argocd_host --insecure --username admin --password $argocd_password --grpc-web
 argocd app create sampleapp --repo git@github.com:agentp2210/dotnet-AKS.git --path "helm/sampleapp" --dest-namespace sampleapp --dest-server https://kubernetes.default.svc --helm-set image.repository="$acr_url/sampleapp"
-argocd app create argo-cd --helm-chart oci://ghcr.io/argoproj/argo-helm/argo-cd --dest-namespace argocd \
+argocd app create argo-cd --repo https://argoproj.github.io/argo-helm --helm-chart argo-cd --dest-namespace argocd \
 --dest-server https://kubernetes.default.svc --helm-set server.ingress.enabled="true" \
---helm-set server.ingress.ingressClassName="nginx" --helm-set global.domain="argocd.anhalan.nl" \
+--helm-set server.ingress.ingressClassName="nginx" --helm-set global.domain=$argocd_host \
 --helm-set configs.params."server\.insecure"=true
 
 # Output
